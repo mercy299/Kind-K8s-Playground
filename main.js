3ad636ccc9191445bb4656e7976386f444079afc
Samuel Aniekeme <mercysam299@gmail.com>
2023-09-20 16:51:57 +0000

    made a change

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World, This is Favour!')
+  res.send('Hello World, This is Aniekeme!')
 })
 
 app.listen(port, () => {
